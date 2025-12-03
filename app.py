37a91f7f442094426fcb98cf39e12f9bd6424e4b
Soufiane TALIBI <TALIBI01@castel.afrique>
2025-12-03 15:57:55 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    user_name = request.headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
+    user_id   = request.headers.get("X-MS-CLIENT-PRINCIPAL-ID")
+
+    if not user_name:
+        return "Utilisateur non authentifié", 401
+
+    return f"Bonjour {user_name} (ID: {user_id}) !"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
