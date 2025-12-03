07e427564113425fd0aaac7116087fb3b206ded8
Soufiane TALIBI <TALIBI01@castel.afrique>
2025-12-03 16:57:13 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, request, jsonify
+﻿from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,3 @@ def home():
         return "Utilisateur non authentifié", 401
 
     return f"Bonjour {user_name} (ID: {user_id}) !"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
